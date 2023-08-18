93f4151f9c40f2356b7e2ec48115c3466a3d48bc
Stenstromen <kontakt@filipstenstrom.se>
2023-08-18 19:08:45 +0200

    Added Email

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
 import { TfiGithub } from "react-icons/tfi";
 
@@ -25,6 +26,15 @@ function Footer() {
               aria-label="GitHub"
             >
               <TfiGithub size={25} />
+            </a>
+            <a
+              href="mailto:info@stenstromen.se?subject=Hello!&body=Hello,%20friend.%0A%0AI%20would%20like%20to%20ask%20you..."
+              rel="noreferrer"
+              target="_blank"
+              className="FooterLink"
+              aria-label="Email"
+            >
+              <MdEmail size={25} />
             </a>
           </div>
         </div>
