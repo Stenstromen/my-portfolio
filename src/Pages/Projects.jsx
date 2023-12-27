14c3a9a48a78bb6b559cd0ea303171829ca5b762
Stenstromen <kontakt@filipstenstrom.se>
2023-12-27 02:11:02 +0100

    Added move to top button

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,19 +1,19 @@
-import React, {useState, useEffect, lazy, Suspense} from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import { motion } from "framer-motion";
 import ProjectList from "./ProjectList";
+import ScrollToTop from "react-scroll-up";
+import { TbSquareArrowUpFilled } from "react-icons/tb";
 
 function Projects() {
   const [isHydrated, setIsHydrated] = useState(false);
-  const ProjectCard = lazy(() => import('../Components/ProjectCard'));
+  const ProjectCard = lazy(() => import("../Components/ProjectCard"));
 
   useEffect(() => {
     setIsHydrated(true);
   }, []);
-  
+
   return (
-    <div
-      className="Projects"
-    >
+    <div className="Projects">
       <motion.div
         initial={{ y: "+1000px", opacity: 0 }}
         animate={{ y: 0, opacity: isHydrated ? 1 : 0 }}
@@ -28,21 +28,24 @@ function Projects() {
           ({ image, title, description, link, github, badges }) => {
             return (
               <>
-              <Suspense key={title} fallback={<div>Loading...</div>}>
-                <ProjectCard
-                  key={title}
-                  image={image}
-                  title={title}
-                  description={description}
-                  link={link}
-                  github={github}
-                  badges={badges}
-                />
+                <Suspense key={title} fallback={<div>Loading...</div>}>
+                  <ProjectCard
+                    key={title}
+                    image={image}
+                    title={title}
+                    description={description}
+                    link={link}
+                    github={github}
+                    badges={badges}
+                  />
                 </Suspense>
               </>
             );
           }
         )}
+        <ScrollToTop showUnder={160}>
+        <TbSquareArrowUpFilled size={45} color="#f686bd"/>
+        </ScrollToTop>
       </motion.div>
     </div>
   );
