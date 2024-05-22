af1ef8a573f111cd76cdc15cd88e8406b635778c
Stenstromen <kontakt@filipstenstrom.se>
2024-05-22 20:28:43 +0200

    added more lazyloading

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
-/* eslint-disable no-undef */
 import React, { lazy, Suspense } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Projects from "./Pages/Projects";
-import Home from "./Pages/Home";
-import Contact from "./Pages/Contact";
-import K8s from "./Pages/K8s";
-import DetBlevIngenCD from "./Pages/DetBlevIngenCD";
-import PGP from "./Pages/PGP";
-import NotFound from "./Pages/NotFound";
 
 function App() {
   const NavBar = lazy(() => import("./Pages/NavBar"));
+  const Projects = lazy(() => import("./Pages/Projects"));
+  const Home = lazy(() => import("./Pages/Home"));
+  const Contact = lazy(() => import("./Pages/Contact"));
+  const K8s = lazy(() => import("./Pages/K8s"));
+  const DetBlevIngenCD = lazy(() => import("./Pages/DetBlevIngenCD"));
+  const PGP = lazy(() => import("./Pages/PGP"));
+  const NotFound = lazy(() => import("./Pages/NotFound"));
   const Footer = lazy(() => import("./Pages/Footer"));
 
   return (
     <div className="AppContainer">
-      <Router /* basename={process.env.PUBLIC_URL} */>
+      <Router>
         <Suspense fallback={<div>Loading...</div>}>
           <NavBar />
-        </Suspense>
-        <div className="MainContent">
-          <Routes>
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/k8s" element={<K8s />} />
-            <Route path="/det-blev-ingen-cd" element={<DetBlevIngenCD />} />
-            <Route path="/pgp" element={<PGP />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-        <Suspense fallback={<div>Loading...</div>}>
+          <div className="MainContent">
+            <Routes>
+              <Route path="/Projects" element={<Projects />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/Contact" element={<Contact />} />
+              <Route path="/k8s" element={<K8s />} />
+              <Route path="/det-blev-ingen-cd" element={<DetBlevIngenCD />} />
+              <Route path="/pgp" element={<PGP />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
           <Footer />
         </Suspense>
       </Router>
