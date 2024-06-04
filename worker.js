7b52d1cb739e21a7b8576c3437c0ba61e40f18e3
Stenstromen <kontakt@filipstenstrom.se>
2024-06-04 13:19:44 +0200

    added csp worker

--- worker.js
+++ worker.js
@@ -1,43 +1,13 @@
 addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
-  });
-  
-  async function handleRequest(request) {
-    const response = await fetch(request);
-    const newResponse = new Response(response.body, response);
-  
-    const nonce = generateNonce();
-  
-    const csp = newResponse.headers.get("Content-Security-Policy");
-    if (csp) {
-      const updatedCsp = csp.replace(
-        "script-src 'self'",
-        `script-src 'self' 'nonce-${nonce}'`
-      );
-      newResponse.headers.set("Content-Security-Policy", updatedCsp);
-    }
-  
-    const body = await response.text();
-    const modifiedBody = addNonceToInlineScript(body, nonce);
-  
-    return new Response(modifiedBody, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: newResponse.headers,
-    });
-  }
-  
-  function generateNonce() {
-    let nonce = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (let i = 0; i < 16; i++) {
-      nonce += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return nonce;
-  }
-  
-  function addNonceToInlineScript(body, nonce) {
-    return body.replace(/<script>(\(function\(\){.*}\)\(\);)<\/script>/, `<script nonce="${nonce}">$1</script>`);
-  }
-  
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  let response = await fetch(request);
+  let newResponse = new Response(response.body, response);
+  let nonce = Math.random().toString(36).substring(2, 15);
+  let csp = newResponse.headers.get("Content-Security-Policy");
+  csp = csp.replace("script-src 'self'", `script-src 'self' 'nonce-${nonce}'`);
+  newResponse.headers.set("Content-Security-Policy", csp);
+  return newResponse;
+}
