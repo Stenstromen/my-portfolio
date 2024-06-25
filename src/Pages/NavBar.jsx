60601540263556c8a85e43bc984f510491381deb
Stenstromen <kontakt@filipstenstrom.se>
2024-06-25 13:52:12 +0200

    added smaller navbar on scroll

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-//import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
 
 function NavBar() {
+  let lastScrollTop = 0;
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+        setIsScrollingDown(true);
+      } else {
+        setIsScrollingDown(false);
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   return (
     <div className="Navbar">
-      <nav className="Nav-font navbar navbar-expand navbar-light fixed-top">
+      <nav className={`Nav-font navbar navbar-expand navbar-light fixed-top ${isScrollingDown ? "Nav-downscroll" : ""}`}>
         <Container className="justify-content-center">
           <Nav className="Nav-filler">
             <LinkContainer to="/">
